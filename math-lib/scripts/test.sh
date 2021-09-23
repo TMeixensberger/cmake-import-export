3182eec39ffacb2b7ba276223e3a474a662b90fe
Tobias Meixensberger <tobias.meixensberger@gmail.com>
2021-09-23 18:25:13 +0200

    moved build and export into a common subdirectory

--- math-lib/scripts/test.sh
+++ math-lib/scripts/test.sh
@@ -1,7 +1,16 @@
-cmake .. -B ../build -DCMAKE_INSTALL_PREFIX:PATH="../Export"
+outputDir="../output"
+buildDir="${outputDir}/build"
+exportDir="${outputDir}/export"
+if [[ ! -d "$outputDir" ]]; then
 
-cmake -DCMAKE_BUILD_TYPE=Debug ../build
-cmake --build ../build --config Debug --target install
+  mkdir $outputDir
 
-cmake -DCMAKE_BUILD_TYPE=Release ../build
-cmake --build ../build --config Release --target install 
+fi
+
+cmake .. -B $buildDir -DCMAKE_INSTALL_PREFIX:PATH=$exportDir
+
+cmake -DCMAKE_BUILD_TYPE=Debug $buildDir
+cmake --build $buildDir --config Debug --target install
+
+cmake -DCMAKE_BUILD_TYPE=Release $buildDir
+cmake --build $buildDir --config Release --target install 
