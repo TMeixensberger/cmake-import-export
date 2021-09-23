4aa16c9fd8289062cbea5e5e9014c766738ba13b
Tobias Meixensberger <tobias.meixensberger@tsep.com>
2021-09-23 11:24:55 +0200

    added export configuration

--- math-lib/scripts/test.cmd
+++ math-lib/scripts/test.cmd
@@ -1,8 +1,9 @@
-cmake .. -B ../build
-cmake -DCMAKE_INSTALL_PREFIX:PATH="../Export" ../build
+@echo off
+
+cmake .. -B ../build -DCMAKE_INSTALL_PREFIX:PATH="../Export"
 
 cmake -DCMAKE_BUILD_TYPE=Debug ../build
-cmake --build ../build --config Debug --target install 
+cmake --build ../build --config Debug --target install
 
 REM cmake -DCMAKE_BUILD_TYPE=Release ../build
 REM cmake --build ../build --config Release --target install 
